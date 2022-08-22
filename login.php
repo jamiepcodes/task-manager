d4db60fd8982069b8d5eaff62b22ecdda8c5468e
jamiepcodes <jamiempounds@gmail.com>
2022-08-22 16:15:19 +0100

    code to test if user details are being passed from login page to back end

--- login.php
+++ login.php
@@ -1,10 +1,18 @@
 <?php 
-
 require 'dbConnect.php';
+session_start();
 
-
-$password = $_POST['user_password'];
+$username =$_POST['username'];
+$password = $_POST['password'];
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+try {
+    $query = "SELECT * FROM users WHERE user_id=? ";
+    $stmt = $pdo->prepare($query);
+    $stmt->execute($username);
+} catch(PDOException $e) {
+    echo "Error : " . $e->getMessage();
+}
 
 
 
